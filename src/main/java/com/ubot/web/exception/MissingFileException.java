140d1e5bb4c7660e16ddcdd81505d52612f989e2
azhe922 <zsda5858sda@gmail.com>
2021-09-01 17:46:31 +0800

    新增查無檔案例外之log

--- src/main/java/com/ubot/web/exception/MissingFileException.java
+++ src/main/java/com/ubot/web/exception/MissingFileException.java
@@ -1,5 +1,8 @@
 package com.ubot.web.exception;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
@@ -7,6 +10,7 @@ import jakarta.ws.rs.ext.Provider;
 @Provider
 public class MissingFileException extends Exception implements ExceptionMapper<MissingFileException> {
 	private static final long serialVersionUID = 1L;
+	private final Logger logger = LogManager.getLogger(this.getClass());
 
 	public MissingFileException() {
 		super("此檔案不存在");
@@ -18,6 +22,7 @@ public class MissingFileException extends Exception implements ExceptionMapper<M
 
 	@Override
 	public Response toResponse(MissingFileException exception) {
+		logger.error(exception.getMessage());
 		return Response.status(404).entity(exception.getMessage()).type("text/plain ;charset=UTF-8").build();
 	}
 }
