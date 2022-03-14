c70e19b85e156fc42f7a01a9b1abbd15d5f3cf01
azhe922 <zsda5858sda@gamil.com>
2022-03-14 09:26:59 +0800

    調整註解

--- src/main/java/com/ubot/web/config/Config.java
+++ src/main/java/com/ubot/web/config/Config.java
@@ -5,12 +5,10 @@ import java.io.IOException;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
-import jakarta.ws.rs.container.PreMatching;
 import jakarta.ws.rs.ext.Provider;
 
-// 在http request進來後第一個進來的class，之後才會pass到service
+// CORS設定
 @Provider
-@PreMatching
 public class Config implements ContainerResponseFilter {
 
 	public void filter(ContainerRequestContext req, ContainerResponseContext res) throws IOException {
